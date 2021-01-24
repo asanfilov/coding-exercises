aa0c265a080a1a072b87133677129faee5e1e317
asanfilov <25196996+asanfilov@users.noreply.github.com>
2021-01-24 15:31:19 -0400

    Solve OneAway [CtCI]. Add unit tests (all passed)

--- CodingExercises/Strings/OneAway.cs
+++ CodingExercises/Strings/OneAway.cs
@@ -6,7 +6,44 @@ namespace CodingExercises
     {
         public bool IsOneAway(string first, string second)
         {
-            return false;
+            if (Math.Abs(first.Length - second.Length) > 1)
+            {
+                return false;
+            }
+
+            if (first.Length <= second.Length)
+            {
+                return compareStrings(first, second);
+            }
+            else
+            {
+                return compareStrings(second, first);
+            }
+        }
+
+        private bool compareStrings(string notLonger, string other)
+        {
+            int nlPos = 0;
+            int oPos = 0;
+            int edits = 0;
+            while (nlPos < notLonger.Length)
+            {
+                if (notLonger[nlPos] != other[oPos])
+                {
+                    edits++;
+                    if (edits > 1)
+                    {
+                        return false;
+                    }
+                    if (notLonger.Length < other.Length)
+                    {
+                        oPos++;
+                    }
+                }
+                nlPos++;
+                oPos++;
+            }
+            return (edits <= 1);
         }
     }
 }
--- CodingExercisesTests/Strings/OneAwayTests.cs
+++ CodingExercisesTests/Strings/OneAwayTests.cs
@@ -5,10 +5,46 @@ namespace CodingExercises.Tests
     [TestFixture()]
     public class OneAwayTests
     {
+        OneAway oneAway = new OneAway();
+
         [Test()]
-        public void IsOneAwayTest()
+        public void IsOneAway_RemovedCharInside_True()
         {
-            Assert.Fail();
+            Assert.True(oneAway.IsOneAway(
+                "sale",
+                "sle"));
+        }
+
+        [Test()]
+        public void IsOneAway_AddedEndingChar_True()
+        {
+            Assert.True(oneAway.IsOneAway(
+                "sale",
+                "sales"));
+        }
+
+        [Test()]
+        public void IsOneAway_ReplacedStartingChar_True()
+        {
+            Assert.True(oneAway.IsOneAway(
+                "sale",
+                "bale"));
+        }
+
+        [Test()]
+        public void IsOneAway_ReplacedTwoChars_False()
+        {
+            Assert.False(oneAway.IsOneAway(
+                "sale",
+                "bake"));
+        }
+
+        [Test()]
+        public void IsOneAway_LengthDiffersByMoreThanOne_False()
+        {
+            Assert.False(oneAway.IsOneAway(
+                "quick",
+                "quickly"));
         }
     }
 }
